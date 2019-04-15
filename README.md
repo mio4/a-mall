f19a576cd233fa1d7a00cdd9131b5cfb7cdbc9d5
mio4 <mio_180@163.com>
2019-04-16 00:24:58 +0800

    start to learn springboot

---
+++ README.md
@@ -0,0 +1,19 @@
+目的：通过从零开始搭建一个B2C电商项目学习前沿技术的基础知识
+
+预期相关技术：
+```
+SpringBoot
+SpringCloud
+Nginx
+Elasticsearch
+RabbitMQ
+Redis
+```
+
+
+备注：
+
+```
+maven仓库 C:...m2
+
+```
---
+++ springbootdemo/src/main/java/com/mio4/web/BootDemoApplication.java
@@ -0,0 +1,11 @@
+package com.mio4.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootDemoApplication.class,args);
+    }
+}
