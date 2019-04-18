4a71b860359ed4cd90ae4f64ff9aab798bbbf7af
mio4 <mio_180@163.com>
2019-04-19 00:54:06 +0800

    update

--- springbootdemo/src/main/java/com/mio4/BootDemoApplication.java
+++ springbootdemo/src/main/java/com/mio4/BootDemoApplication.java
@@ -1,9 +1,12 @@
-package com.mio4.web;
+package com.mio4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+//@ComponentScans()
 public class BootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootDemoApplication.class,args);
--- springbootdemo/src/main/java/com/mio4/web/HelloController.java
+++ springbootdemo/src/main/java/com/mio4/web/HelloController.java
@@ -1,11 +1,18 @@
 package com.mio4.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
+
+
 @Controller
 public class HelloController {
+
+    @Autowired
+    private DataSource dataSource;
 
     @GetMapping("hello")
     @ResponseBody
