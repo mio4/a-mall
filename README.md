ae4f72844a2c96a08bf0d9f1d97b5eccca801d14
mio4 <mio_180@163.com>
2019-04-22 09:00:46 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## 目的：通过从零开始搭建一个B2C电商项目学习前沿技术的基础知识
 
-预期相关技术：
-```
+```cmd
+# 技能栈
 SpringBoot
 SpringCloud
 Nginx
@@ -11,23 +11,23 @@ Redis
 ```
 
 ## 学习进度：
-Update Time : 2019/4/21
+Update Time : 2019/4/22
 
 1. SpringBoot自动配置原理——yaml文件配置
 2. SpringMVC——拦截器配置、Slf4j日志、通用Mapper配置、注解复习
 3. 系统架构：集中式架构——>SOA架构——>微服务架构（RPC | HTTP）
 4. Spring访问Rest服务的客户端——RestTemplate
 5. SpringCloud入门——基于SpringBoot的微服务容器
-	父子工程——服务提供方&服务接收方
+  父子工程——服务提供方&服务接收方
 6. Eureka服务管理中心——能够管理消费者-生产者模型：pom.xml中添加maven依赖，添加@EnableDiscoveryClient注解，配置application.yml文件——改进：Consumer动态获取Service地址——搭建Eureka中心集群（eg：两个eureka-server，两个eureka-client）
+7. Eureka——Ribbon负载均衡 | 对于Ribbon源码的分析：轮询算法&随机算法
+8. Histrix熔断器
 
 
 
 
 
 
-
----
 
 note：
 
--- cloud-demo/user-consumer/src/main/java/com/mio4/consumer/controller/ConsumerController.java
+++ cloud-demo/user-consumer/src/main/java/com/mio4/consumer/controller/ConsumerController.java
@@ -4,6 +4,7 @@ import com.mio4.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,16 +22,20 @@ public class ConsumerController {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+//    private RibbonLoadBalancerClient client;
 
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id){
         //获取实例
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+//        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
         //从实例中获取端口和IP
-        ServiceInstance instance = instances.get(0);
-        String url = "http://" + instance.getHost()  + ":" + instance.getPort() + "/user/" + id;
+//        ServiceInstance instance = instances.get(0);
+//        String url = "http://" + instance.getHost()  + ":" + instance.getPort() + "/user/" + id;
 //        String url = "http://localhost:8081/user/" + id;
 
+//        ServiceInstance instance = client.choose("user-service");
+
+        String url = "http://user-service/user/" + id;
         User user = restTemplate.getForObject(url,User.class);
         return user;
     }
